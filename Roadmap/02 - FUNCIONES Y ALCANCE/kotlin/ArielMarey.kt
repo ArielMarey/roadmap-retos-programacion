390b4e6c96f47f7c7786f5b9a22420a3bcb50874
ArielMarey <43869270+ArielMarey@users.noreply.github.com>
2025-07-28 15:58:28 -0400

    Update ArielMarey.kt

--- Roadmap/02 - FUNCIONES Y ALCANCE/kotlin/ArielMarey.kt
+++ Roadmap/02 - FUNCIONES Y ALCANCE/kotlin/ArielMarey.kt
@@ -19,5 +19,69 @@
  
   Presta especial atención a la sintaxis que debes utilizar en cada uno de los casos.
   Cada lenguaje sigue una convenciones que debes de respetar para que el código se entienda.
- */
+  */
 
+// Variables globales
+
+val instCuerda = listOf("Violin", "Viola", "Cello", "Contrabajo")
+
+// Ejemplos de funciones
+
+fun nombrarInstr1() { 										// funcion sin parametros
+	for (instrumento in instCuerda) {
+        println(instrumento)
+    }
+}
+
+fun nombrarInstr(instrumento: String){						// funcion con parametros
+    val instCheck = instrumento in instCuerda 				// variable local
+    println("La orquesta tiene $instrumento? $instCheck")
+}
+
+fun getInstr(): String {									// funcion con return
+    return instCuerda.find {it=="Violin"}.toString()
+}
+
+fun getInstSize() {											// funcion dentro de otra funcion
+    fun instSize() {
+        val size = mapOf("Violin" to "chico", "Viola" to "mediano", "Cello" to "grande", "Contrabajo" to "Gigante")
+        for ((instCuerda, size) in size) {
+            println("El $instCuerda es $size")
+        }
+    }
+    println(instSize())
+}
+
+fun cusFizzBuzz(word1: String = "Fizz", word2: String = "Buzz"): Int {
+    
+    var w1Quant: Int = 0
+    var w2Quant: Int = 0
+    var w3Quant: Int = 0
+    var nuQuant: Int = 0
+
+    for (x in 1..100) {
+        when {
+            x % 15 == 0 -> {println("$word1$word2"); w3Quant++}
+            x % 3 == 0 -> {println("$word1"); w1Quant++}      
+            x % 5 == 0 -> {println("$word2"); w2Quant++}                      
+         else -> 
+            {println("$x"); nuQuant++}           
+        }
+    }
+    println("La palabra $word1 se dijo $w1Quant veces.") 
+    println("La palabra $word2 se dijo $w2Quant veces.")
+    println("La combinacion de ambas se dijo $w3Quant veces.")
+    println("Los numeros por si solos aparecieron $nuQuant veces.")
+    return nuQuant
+}
+
+fun main() {							// funcion "main"
+    println(nombrarInstr1())
+    println(nombrarInstr("Viola"))
+    println(getInstr())
+    println(getInstSize())
+    println(cusFizzBuzz("Manzana", "Pera"))
+    
+}
+   
+    
